e98396b989075292fddfd6579008ae65498f962a
Daniel Rothwell <danielrothwell@Daniels-MacBook-Pro.local>
2022-07-25 15:12:59 -0400

    hello

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.send('Hello')
 });
 
+
+router.post( "/post-message",function(req,res,next){
+  try{
+    const dateTime = new Date().toString()
+    const clientMessage = req.body.message;
+    res.status(200).json({success:clientMessage,UploadTime:dateTime})
+
+  }catch(e){res.status(500).json({success:false,message:"Error"})}
+})
 module.exports = router;
