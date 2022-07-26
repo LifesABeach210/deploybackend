c281727eb89d4d44deb838f32d0c7d63a845c9de
Daniel Rothwell <danielrothwell@Daniels-MacBook-Pro.local>
2022-07-26 11:25:42 -0400

    .

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.post( "/post-message",function(req,res,next){
   try{
     const dateTime = new Date().toString()
     const clientMessage = req.body;
-    res.status(200).json({success:clientMessage,UploadTime:dateTime})
+    res.status(200).json({success:`Received client message: ${clientMessage}. Responded at ${dateTime.toString()}`})
 
   }catch(e){res.status(500).json({success:false,message:"Error"})}
 })
