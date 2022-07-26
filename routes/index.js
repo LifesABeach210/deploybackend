e58435c2d513a68abc7ef1eadfcb9b884fdc15c6
Hojong Yu <hojongyu@Hojongs-MBP.home>
2022-07-26 15:34:29 -0400

    user list assignment is updated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const userList = [{
+  id: 1,
+  firstName: "HJ",
+  lastName: "Yu",
+  email: "hjy@gmail.com"
+}];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
  res.send('Hello')
@@ -15,5 +22,15 @@ router.post( "/post-message",function(req,res,next){
     res.status(200).json({success:`Received client message: ${clientMessage}. Responded at ${dateTime.toString()}`})
 
   }catch(e){res.status(500).json({success:false,message:"Error"})}
+})
+
+router.get('/get-users',function(req, res, next){
+  try{
+    res.json({message:userList}).status(200)
+    
+  }catch(e){
+    console.log(e)
+    res.status(500).send('error fetching data ' + e)
+  }
 })
 module.exports = router;
